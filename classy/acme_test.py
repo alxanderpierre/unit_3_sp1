1c3d9bd435e26f827774f1a464b9fe29050d1ab7
alxanderpierre <alxanderpierre@users.noreply.github.com>
2019-10-04 16:28:14 -0400

    dfd

--- classy/acme_test.py
+++ classy/acme_test.py
@@ -24,3 +24,22 @@ class AcmeReportTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+# What, in your opinion, is an important part of code reviews? That is, what is
+#  something you pay attention to when you review code, and that you appreciate
+#  when others do the same for your code?
+
+# ANSWER: One thing that is very important to code review and something
+# that i appreciate is when others show me how to make my code more DRY and less
+# WET. Its important to have dry code because it important to repeat yourself
+# too many time. Code is meant to be shared. If you have
+# wet code it makes it harder for other to read and interpret your code.
+
+# We have an awful lot of computers here, and it gets pretty confusing with
+#  slightly different things running on all of them. How could containers help us
+#  improve this situation?
+
+# ANSWER: Containers can be very very very useful in situations like these. Using 
+# applications like docker allows a person to share their code with other and
+# it will automatically import the correct dependencies based on the machine it
+# is going to.
